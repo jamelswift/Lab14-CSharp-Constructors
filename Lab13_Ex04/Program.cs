3b626fc08c0ed26e44d018ca78eca0d995a0d980
jamelswift <Wipatsasicha0702@gmail.com>
2025-03-07 17:46:12 +0700

    66030281 วิภัสศศิชา

---
+++ Lab13_Ex04/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Shape
+{
+    private int? NumOfSide;
+    
+    // เปลี่ยนเป็น protected เพื่อให้คลาสลูกสามารถเข้าถึงได้
+    protected Shape()
+    {
+        System.Console.WriteLine("This is some shape with unknown side");
+    }
+
+    public Shape(int NumOfSide)
+    {
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base() // เรียกคอนสตรัคเตอร์ของ Shape
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4) // เรียกคอนสตรัคเตอร์ของ Shape ที่รับพารามิเตอร์
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+    public Triangle() : base(3) // เรียกคอนสตรัคเตอร์ของ Shape ที่รับพารามิเตอร์
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var circle = new Circle();
+        var rectangle = new Rectangle();
+        var triangle = new Triangle();
+    }
+}
---
+++ Lab13_Ex05/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Shape
+{
+    private int? NumOfSide;
+    
+    private Shape() // คอนสตรัคเตอร์ private ที่ไม่สามารถเรียกใช้จากคลาสลูก
+    {
+    }
+    
+    // คอนสตรัคเตอร์ที่รับพารามิเตอร์
+    public Shape(int NumOfSide) : this()
+    {
+        this.NumOfSide = NumOfSide;
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base(0) // Circle มีจำนวนด้าน 0
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4) // Rectangle มีจำนวนด้าน 4
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+   public Triangle() : base(3) // Triangle มีจำนวนด้าน 3
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var circle = new Circle();
+        var rectangle = new Rectangle();
+        var triangle = new Triangle();
+    }
+}
